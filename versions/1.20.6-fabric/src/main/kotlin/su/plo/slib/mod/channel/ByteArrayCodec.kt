db8cc763772f62ffbe1dcf3d1871033295361416
Apehum <apehumchik@gmail.com>
2024-04-29 13:18:28 +0800

    fix: use RegistryFriendlyByteBuf#readableBytes for reading bytearray payload

--- versions/1.20.6-fabric/src/main/kotlin/su/plo/slib/mod/channel/ByteArrayCodec.kt
+++ versions/1.20.6-fabric/src/main/kotlin/su/plo/slib/mod/channel/ByteArrayCodec.kt
@@ -6,13 +6,13 @@ import net.minecraft.network.protocol.common.custom.CustomPacketPayload
 import net.minecraft.resources.ResourceLocation
 
 class ByteArrayCodec(
-    private val channelKey: ResourceLocation
+    channelKey: ResourceLocation
 ) : StreamCodec<RegistryFriendlyByteBuf, ByteArrayPayload> {
 
     val type = CustomPacketPayload.Type<ByteArrayPayload>(channelKey)
 
     override fun decode(buf: RegistryFriendlyByteBuf): ByteArrayPayload {
-        val length = buf.readVarInt()
+        val length = buf.readableBytes()
 
         val data = ByteArray(length)
         buf.readBytes(data)
@@ -21,7 +21,6 @@ class ByteArrayCodec(
     }
 
     override fun encode(buf: RegistryFriendlyByteBuf, payload: ByteArrayPayload) {
-        buf.writeVarInt(payload.data.size)
         buf.writeBytes(payload.data)
     }
 }
