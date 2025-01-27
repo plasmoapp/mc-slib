66dc7735312dc9b7cd4aab5c9e2daf58a14607e8
Apehum <apehum@proton.me>
2025-01-27 17:15:50 +0800

    fix(spigot): send error message if game events are not available

--- spigot/src/main/kotlin/su/plo/slib/spigot/world/SpigotServerWorld.kt
+++ spigot/src/main/kotlin/su/plo/slib/spigot/world/SpigotServerWorld.kt
@@ -4,11 +4,13 @@ import org.bukkit.World
 import org.bukkit.entity.Entity
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.util.Vector
+import su.plo.slib.api.logging.McLoggerFactory
 import su.plo.slib.api.server.entity.McServerEntity
 import su.plo.slib.api.server.world.McServerWorld
 import su.plo.slib.spigot.extension.runSync
 import su.plo.slib.spigot.util.GameEventUtil.parseGameEvent
-import java.util.*
+import java.util.Objects
+import java.util.concurrent.atomic.AtomicBoolean
 
 class SpigotServerWorld(
     private val loader: JavaPlugin,
@@ -21,17 +23,23 @@ class SpigotServerWorld(
         val gameEventClass = try {
             Class.forName("org.bukkit.GameEvent")
         } catch (e: ClassNotFoundException) {
+            sendGameEventNotSupported()
             return
         }
 
         val paperEntity = entity.getInstance<Entity>()
         loader.runSync(paperEntity) {
-            val sendGameEventMethod = level.javaClass.getMethod(
-                "sendGameEvent",
-                Entity::class.java,
-                gameEventClass,
-                Vector::class.java
-            )
+            val sendGameEventMethod = try {
+                level.javaClass.getMethod(
+                    "sendGameEvent",
+                    Entity::class.java,
+                    gameEventClass,
+                    Vector::class.java
+                )
+            } catch (e: NoSuchMethodException) {
+                sendGameEventNotSupported()
+                return@runSync
+            }
 
             sendGameEventMethod.invoke(level, paperEntity, parseGameEvent(gameEvent), paperEntity.location.toVector())
         }
@@ -50,4 +58,14 @@ class SpigotServerWorld(
         }
 
     override fun hashCode() = Objects.hash(level)
+
+    companion object {
+        private val logger = McLoggerFactory.createLogger("SpigotServerWorld")
+        private var sent: AtomicBoolean = AtomicBoolean(false)
+
+        private fun sendGameEventNotSupported() {
+            if (!sent.compareAndSet(false, true)) return
+            logger.error("Game events are not supported on your platform. Update to 1.19.2+ and use Paper: https://papermc.io/.")
+        }
+    }
 }
