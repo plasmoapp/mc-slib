315de3ea34a08f39619c50ba0e39430fa8d449f6
Apehum <apehum@proton.me>
2025-03-29 02:43:53 +0800

    fix(spigot): cache game events

--- spigot/src/main/kotlin/su/plo/slib/spigot/util/GameEventUtil.kt
+++ spigot/src/main/kotlin/su/plo/slib/spigot/util/GameEventUtil.kt
@@ -1,22 +1,64 @@
 package su.plo.slib.spigot.util
 
 import org.bukkit.NamespacedKey
+import org.bukkit.World
+import org.bukkit.entity.Entity
+import org.bukkit.util.Vector
+import su.plo.slib.api.logging.McLoggerFactory
+import java.util.concurrent.atomic.AtomicBoolean
 
 object GameEventUtil {
 
-    fun parseGameEvent(gameEventName: String): Any {
-        val gameEventKey: NamespacedKey
-        val split = gameEventName.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-        gameEventKey = if (split.size == 2) {
-            NamespacedKey(split[0], split[1])
-        } else {
-            NamespacedKey("minecraft", gameEventName)
+    private val gameEvents: MutableMap<String, Any> = HashMap()
+
+    private val gameEventClass by lazy {
+        try {
+            Class.forName("org.bukkit.GameEvent")
+        } catch (e: ClassNotFoundException) {
+            sendGameEventNotSupported()
+            null
         }
-
-        val gameEventClass = Class.forName("org.bukkit.GameEvent")
-        val gameEventByKeyMethod = gameEventClass.getMethod("getByKey", NamespacedKey::class.java)
-
-        return gameEventByKeyMethod.invoke(null, gameEventKey)
-            ?: return gameEventClass.getField("STEP").get(null)
     }
+
+    private val sendEntityGameEventMethod by lazy {
+        try {
+            World::class.java.getMethod(
+                "sendGameEvent",
+                Entity::class.java,
+                gameEventClass,
+                Vector::class.java
+            )
+        } catch (e: NoSuchMethodException) {
+            sendGameEventNotSupported()
+            null
+        }
+    }
+
+    private val logger = McLoggerFactory.createLogger("GameEventUtil")
+    private var sent: AtomicBoolean = AtomicBoolean(false)
+
+    private fun sendGameEventNotSupported() {
+        if (!sent.compareAndSet(false, true)) return
+        logger.error("Game events are not supported on your platform. Update to 1.19.2+ and use Paper: https://papermc.io/.")
+    }
+
+    fun World.sendEntityGameEvent(paperEntity: Entity, gameEventName: String) {
+        sendEntityGameEventMethod?.invoke(this, paperEntity, parseGameEvent(gameEventName), paperEntity.location.toVector())
+    }
+
+    fun parseGameEvent(gameEventName: String): Any =
+        gameEvents.computeIfAbsent(gameEventName) {
+            val gameEventKey: NamespacedKey
+            val split = gameEventName.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            gameEventKey = if (split.size == 2) {
+                NamespacedKey(split[0], split[1])
+            } else {
+                NamespacedKey("minecraft", gameEventName)
+            }
+
+            val gameEventClass = Class.forName("org.bukkit.GameEvent")
+            val gameEventByKeyMethod = gameEventClass.getMethod("getByKey", NamespacedKey::class.java)
+
+            gameEventByKeyMethod.invoke(null, gameEventKey) ?: gameEventClass.getField("STEP").get(null)
+        }
 }
--- spigot/src/main/kotlin/su/plo/slib/spigot/world/SpigotServerWorld.kt
+++ spigot/src/main/kotlin/su/plo/slib/spigot/world/SpigotServerWorld.kt
@@ -3,14 +3,11 @@ package su.plo.slib.spigot.world
 import org.bukkit.World
 import org.bukkit.entity.Entity
 import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.util.Vector
-import su.plo.slib.api.logging.McLoggerFactory
 import su.plo.slib.api.server.entity.McServerEntity
 import su.plo.slib.api.server.world.McServerWorld
 import su.plo.slib.spigot.extension.runSync
-import su.plo.slib.spigot.util.GameEventUtil.parseGameEvent
+import su.plo.slib.spigot.util.GameEventUtil.sendEntityGameEvent
 import java.util.Objects
-import java.util.concurrent.atomic.AtomicBoolean
 
 class SpigotServerWorld(
     private val loader: JavaPlugin,
@@ -20,28 +17,9 @@ class SpigotServerWorld(
     override val name: String = level.name
 
     override fun sendGameEvent(entity: McServerEntity, gameEvent: String) {
-        val gameEventClass = try {
-            Class.forName("org.bukkit.GameEvent")
-        } catch (e: ClassNotFoundException) {
-            sendGameEventNotSupported()
-            return
-        }
-
         val paperEntity = entity.getInstance<Entity>()
         loader.runSync(paperEntity) {
-            val sendGameEventMethod = try {
-                level.javaClass.getMethod(
-                    "sendGameEvent",
-                    Entity::class.java,
-                    gameEventClass,
-                    Vector::class.java
-                )
-            } catch (e: NoSuchMethodException) {
-                sendGameEventNotSupported()
-                return@runSync
-            }
-
-            sendGameEventMethod.invoke(level, paperEntity, parseGameEvent(gameEvent), paperEntity.location.toVector())
+            level.sendEntityGameEvent(paperEntity, gameEvent)
         }
     }
 
@@ -58,14 +36,4 @@ class SpigotServerWorld(
         }
 
     override fun hashCode() = Objects.hash(level)
-
-    companion object {
-        private val logger = McLoggerFactory.createLogger("SpigotServerWorld")
-        private var sent: AtomicBoolean = AtomicBoolean(false)
-
-        private fun sendGameEventNotSupported() {
-            if (!sent.compareAndSet(false, true)) return
-            logger.error("Game events are not supported on your platform. Update to 1.19.2+ and use Paper: https://papermc.io/.")
-        }
-    }
 }
