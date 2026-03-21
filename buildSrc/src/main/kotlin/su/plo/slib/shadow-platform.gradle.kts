66cd6d5d31dce94c4cb13f5fb8134704ba0962ca
Apehum <apehum@proton.me>
2026-03-21 19:19:27 +0800

    build: remove multi-release from final jar manifest

--- buildSrc/src/main/kotlin/su/plo/slib/shadow-platform.gradle.kts
+++ buildSrc/src/main/kotlin/su/plo/slib/shadow-platform.gradle.kts
@@ -1,5 +1,11 @@
 package su.plo.slib
 
+import java.net.URI
+import java.nio.file.FileSystems
+import java.nio.file.Files
+import java.util.jar.Attributes
+import java.util.jar.Manifest
+
 plugins {
     java
     id("com.gradleup.shadow")
@@ -13,6 +19,23 @@ tasks {
 
         archiveAppendix.set("")
         archiveClassifier.set("")
+
+        exclude("META-INF/versions/**")
+
+        // Shadow 9.x merges Multi-Release from transitive deps (examination, option),
+        // but the actual META-INF/versions classes aren't included, breaking Forge's securejarhandler
+        doLast {
+            val jarFile = archiveFile.get().asFile
+            val uri = URI.create("jar:${jarFile.toURI()}")
+            FileSystems.newFileSystem(uri, mapOf("create" to "false")).use { fs ->
+                val manifestPath = fs.getPath("META-INF", "MANIFEST.MF")
+                val manifest = Manifest()
+                Files.newInputStream(manifestPath).use { manifest.read(it) }
+                if (manifest.mainAttributes.remove(Attributes.Name("Multi-Release")) != null) {
+                    Files.newOutputStream(manifestPath).use { manifest.write(it) }
+                }
+            }
+        }
     }
 
     build {
