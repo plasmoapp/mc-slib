316bd3df2568f082384f10345251ec5cc1dc544d
Apehum <apehum@proton.me>
2026-04-24 04:05:39 +0800

    build: add commands to smoke tests

--- minestom/src/test/kotlin/su/plo/slib/minestom/TestMinestomServer.kt
+++ minestom/src/test/kotlin/su/plo/slib/minestom/TestMinestomServer.kt
@@ -1,5 +1,8 @@
 package su.plo.slib.minestom
 
+import net.kyori.adventure.key.Key
+import net.kyori.adventure.translation.GlobalTranslator
+import net.kyori.adventure.translation.TranslationStore
 import net.minecrell.terminalconsole.SimpleTerminalConsole
 import net.minestom.server.MinecraftServer
 import net.minestom.server.coordinate.Pos
@@ -10,6 +13,9 @@ import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
 import net.minestom.server.event.player.PlayerSpawnEvent
 import su.plo.slib.server.TestServer
 import java.io.File
+import java.text.MessageFormat
+import java.util.Locale
+import kotlin.concurrent.thread
 import kotlin.system.exitProcess
 import kotlin.time.measureTime
 
@@ -22,6 +28,8 @@ fun main() {
 
 fun startServer() {
     val minecraftServer = MinecraftServer.init()
+
+    registerVanillaTranslations()
 
     val minecraftServerLib = MinestomServerLib(File("slib-test"))
     val testServer = TestServer(minecraftServerLib)
@@ -47,7 +55,9 @@ fun startServer() {
 
     minecraftServer.start("0.0.0.0", 25565)
 
-    Console().start()
+    thread(isDaemon = true) {
+        Console().start()
+    }
 }
 
 class Console : SimpleTerminalConsole() {
@@ -79,4 +89,14 @@ class Console : SimpleTerminalConsole() {
 
 private inline fun <reified T : Event> GlobalEventHandler.addListener(crossinline listener: (T) -> Unit) {
     addListener(T::class.java) { listener.invoke(it) }
+}
+
+// Minestom doesn't ship vanilla translations, so translation keys leak to the console as raw ids
+// Register the minimum set the smoke tests need
+private fun registerVanillaTranslations() {
+    val store = TranslationStore.messageFormat(Key.key("slib", "test"))
+    store.defaultLocale(Locale.US)
+    store.register("command.context.parse_error", Locale.US, MessageFormat("{0} at position {1}: {2}"))
+    store.register("argument.uuid.invalid", Locale.US, MessageFormat("Invalid UUID"))
+    GlobalTranslator.translator().addSource(store)
 }
