3065088b6f3bb5ce46d5d1fc31943a5a35fb3441
Apehum <apehum@proton.me>
2026-04-27 10:24:37 +0800

    chore(proxy): align UuidArgumentType with server fixture

--- common-proxy/src/testFixtures/kotlin/su/plo/slib/proxy/command/UuidArgumentType.kt
+++ common-proxy/src/testFixtures/kotlin/su/plo/slib/proxy/command/UuidArgumentType.kt
@@ -1,14 +1,14 @@
 package su.plo.slib.proxy.command
 
-import com.mojang.brigadier.LiteralMessage
 import com.mojang.brigadier.StringReader
 import com.mojang.brigadier.arguments.ArgumentType
 import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.context.CommandContext
-import com.mojang.brigadier.exceptions.CommandSyntaxException
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType
 import com.mojang.brigadier.suggestion.Suggestions
 import com.mojang.brigadier.suggestion.SuggestionsBuilder
+import su.plo.slib.api.chat.component.McTextComponent
+import su.plo.slib.api.chat.converter.MessageTextConverter
 import su.plo.slib.api.command.brigadier.CustomArgumentType
 import java.util.UUID
 import java.util.concurrent.CompletableFuture
@@ -16,25 +16,28 @@ import java.util.concurrent.CompletableFuture
 class UuidArgumentType : CustomArgumentType<UUID, String> {
     override val nativeType: ArgumentType<String> = StringArgumentType.string()
 
-    private val invalidUuid = SimpleCommandExceptionType(LiteralMessage("Failed to parse UUID"))
+    override fun useNativeSuggestions(): Boolean = false
 
-    override fun parse(reader: StringReader): UUID? {
+    private val invalidUuid = SimpleCommandExceptionType(
+        MessageTextConverter.converter().convert(
+            McTextComponent.translatable(
+                "argument.uuid.invalid",
+            )
+        )
+    )
+
+    override fun parse(reader: StringReader): UUID {
         val input = reader.readString()
-            ?.takeIf { it.isNotBlank() }
-            ?: return null
 
         try {
             return UUID.fromString(input)
-        } catch (e: IllegalArgumentException) {
-            throw CommandSyntaxException(invalidUuid, LiteralMessage(e.message), input, 0)
+        } catch (_: IllegalArgumentException) {
+            throw invalidUuid.createWithContext(reader)
         }
     }
 
     override fun <S> listSuggestions(
         context: CommandContext<S>,
         builder: SuggestionsBuilder,
-    ): CompletableFuture<Suggestions> =
-        builder
-            .suggest(UUID.randomUUID().toString())
-            .buildFuture()
+    ): CompletableFuture<Suggestions> = Suggestions.empty()
 }
